78718dcc7fa1fc3c4db075c304b9d4dac0c5a91a
YosstinCode <yosstincode@gmail.com>
2023-06-20 13:29:53 -0500

    Advancements in Contact Form Styles.

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom'
 interface ButtonProps {
     text: string;
     isShadow?: boolean;
-    path: string;
+    path?: string;
 }
 
-const Button = ({ text, isShadow = false, path }: ButtonProps) => {
+const Button = ({ text, isShadow = false, path = '#' }: ButtonProps) => {
   const classButton = 'flex justify-center items-center font-semibold text-sm w-[117px] h-[44px] rounded-[10px] text-center ' + (isShadow ? 'border-2 border-borderShadow text-borderShadow hover:bg-borderShadow hover:text-white' : 'text-white bg-azulito hover:bg-azulitoHover')
 
   return (
--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,8 +1,19 @@
+import Button from '../Button/Button'
+import TextGradientOrWhite from '../TextGradientOrWhite/TextGradient'
+
 const ContactForm = () => {
   return (
-    <form>
-      <input type='text' name='subject' id='subject' />
-      <textarea name='menssage' id='message' cols={30} rows={10} />
+    <form className='flex flex-col justify-center items-center gap-4 rounded-[30px] w-[50rem] h-[32.813rem] mx-auto'>
+      <div className='flex flex-col justify-center items-center'>
+        <TextGradientOrWhite text='Tu idea mi compromiso' />
+        <TextGradientOrWhite text='Trabajemos juntos' isGradient />
+      </div>
+      <div className='flex flex-col gap-8 items-center'>
+
+        <input className='w-[32.75rem] h-10 py-2 px-4 rounded-[20px] bg-transparent' type='text' name='subject' id='subject' placeholder='Escribe tu asunto...' />
+        <textarea className='w-[32.75rem] h-[12.5rem] py-2 px-4 rounded-[20px] bg-transparent resize-none' name='menssage' id='message' cols={30} rows={10} placeholder='Escribe tu mensaje...' />
+      </div>
+      <Button text='Enviar' />
     </form>
   )
 }
